f2126df1db563c56041c41d3e7140bac85a649a2
Haadi <munshi.haadi@gmail.com>
2020-08-18 11:34:10 -0400

    created migrations and models, wrote associations

---
+++ W5D5/polls_app/app/models/response.rb
@@ -0,0 +1,12 @@
+class Response < ApplicationRecord
+
+    belongs_to :answer_choice,
+        primary_key: :id,
+        foreign_key: :answer_choice_id,
+        class_name: :AnswerChoice
+
+    belongs_to :respondent,
+        primary_key: :id,
+        foreign_key: :respondent_id,
+        class_name: :User
+end
